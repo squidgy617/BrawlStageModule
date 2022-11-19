18addaa3c852afc428945ef5779b14dbd8efe099
Kapedani <kapedanipm@gmail.com>
2022-11-19 03:49:39 -0500

    Submodule update

--- custom/st_warfactory/source/gr_warfactory_conveyor.cpp
+++ custom/st_warfactory/source/gr_warfactory_conveyor.cpp
@@ -15,15 +15,15 @@ void grWarFactoryConveyor::startup(gfArchive* archive, u32 unk1, u32 unk2)
 {
     grYakumono::startup(archive, unk1, unk2);
     ConveyorGimmickData* conveyorGimmickData = (ConveyorGimmickData*)this->getGimmickData();
-    BeltConveyorGimmickAreaData beltConveyorGimmickData;
-    __memfill(&beltConveyorGimmickData, 0, sizeof(BeltConveyorGimmickAreaData));
-    beltConveyorGimmickData.m_range = (Vec2f){conveyorGimmickData->range, 10.0};
-    beltConveyorGimmickData.m_speed = conveyorGimmickData->speed;
-    beltConveyorGimmickData.m_isRightDirection = conveyorGimmickData->isRightDirection;
-    beltConveyorGimmickData.m_conveyorPos = (Vec3f){conveyorGimmickData->pos.x,conveyorGimmickData->pos.y,0};
+    SquareBeltConveyorGimmickAreaData beltConveyorAreaData;
+    __memfill(&beltConveyorAreaData, 0, sizeof(SquareBeltConveyorGimmickAreaData));
+    beltConveyorAreaData.m_range = (Vec2f){conveyorGimmickData->range, 10.0};
+    beltConveyorAreaData.m_speed = conveyorGimmickData->speed;
+    beltConveyorAreaData.m_isRightDirection = conveyorGimmickData->isRightDirection;
+    beltConveyorAreaData.m_conveyorPos = (Vec3f){conveyorGimmickData->pos.x,conveyorGimmickData->pos.y,0};
 
     stTrigger* trigger = g_stTriggerMng->createTrigger(Gimmick_Kind_BeltConveyor,-1);
-    trigger->setBeltConveyorTrigger(&beltConveyorGimmickData);
+    trigger->setBeltConveyorTrigger(&beltConveyorAreaData);
 }
 
 void grWarFactoryConveyor::update(float deltaFrame)
