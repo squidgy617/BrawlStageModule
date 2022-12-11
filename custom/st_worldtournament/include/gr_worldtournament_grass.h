93f431629c12ca1f1e3b75013099195dd7514a87
Kapedani <kapedanipm@gmail.com>
2022-12-11 13:53:39 -0500

    [stages/worldtournament] Reimplemented toKnockOut to not have slowdown and white flash effect (and so every character uses same function for knockOut)

--- custom/st_worldtournament/include/gr_worldtournament_grass.h
+++ custom/st_worldtournament/include/gr_worldtournament_grass.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gr/gr_yakumono.h>
+#include <ft/ft_manager.h>
 
 class grWorldTournamentGrass : public grYakumono {
 protected:
@@ -13,6 +14,7 @@ public:
     virtual void update(float deltaFrame);
     virtual void receiveCollMsg_Landing(grCollStatus* collStatus, grCollisionJoint* collisionJoint, bool unk3);
     virtual ~grWorldTournamentGrass(){};
+    virtual void toKnockOut(Fighter* fighter);
 
     static grWorldTournamentGrass* create(int mdlIndex, char* tgtNodeName, char* taskName);
 };
