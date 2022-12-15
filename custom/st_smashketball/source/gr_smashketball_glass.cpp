bfd8c72d786f360ca3f8c53b1f0e71c36b2b455e
Kapedani <kapedanipm@gmail.com>
2022-12-15 03:20:28 -0500

    [custom/smashketball] Fixed barrel and glass updates

--- custom/st_smashketball/source/gr_smashketball_glass.cpp
+++ custom/st_smashketball/source/gr_smashketball_glass.cpp
@@ -10,15 +10,31 @@ grSmashketballGlass* grSmashketballGlass::create(int mdlIndex, char* tgtNodeName
     glass->makeCalcuCallback(1, Heaps::StageInstance);
     glass->setCalcuCallbackRoot(7);
 
-    glass->setupHitPoint();
-
     return glass;
 }
 
+void grSmashketballGlass::startup(gfArchive* archive, u32 unk1, u32 unk2) {
+    this->setupHitPoint();
+
+    grMadein::startup(archive, unk1, unk2);
+
+    this->initializeEntity();
+    this->startEntity();
+
+    this->setHit();
+}
+
 void grSmashketballGlass::setupHitPoint() {
-    Vec3f startOffsetPos = {0,0,0};
-    Vec3f endOffsetPos = {0,0,0};
+    Vec3f startOffsetPos = {-5,0,0};
+    Vec3f endOffsetPos = {5,0,0};
     this->setHitPoint(7.0, &startOffsetPos, &endOffsetPos, 1, 1);
+}
+
+void grSmashketballGlass::setHit() {
+    // Done cause Brawl devs allocated ykData on the stack in grMadein::setupYakumonoClass leading to ykData being a garbage pointer so have to replace it in order to be able to change the HitSelfCategory
+    this->m_yakumono->m_data = &this->yakumonoData;
+    this->setSituationODD();
+    //this->m_yakumono->setCollisionHitSelfCatagory(9); // Changed category so that projectiles like Mario fireball don't get absorbed by it
 }
 
 void grSmashketballGlass::update(float deltaFrame)
