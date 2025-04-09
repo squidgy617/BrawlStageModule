b6c6c6227702e1e2f458b92fead5823f4d8e97bd
Álvaro <alvarofdezneira@gmail.com>
2025-04-09 04:55:07 +0200

    [Phendrana] Changed conditions to enter pinch mode.

--- custom/st_phendrana/source/gr_phendrana_pinch.cpp
+++ custom/st_phendrana/source/gr_phendrana_pinch.cpp
@@ -66,18 +66,36 @@ bool grPhendranaPinch::checkForPinch()
 
     scMelee* scene = static_cast<scMelee*>(gfSceneManager::getInstance()->searchScene("scMelee"));
     stOperatorRuleMelee* operatorRule = static_cast<stOperatorRuleMelee*>(scene->m_operatorRuleGameMode);
+    stOperatorDropItem* operatorDropItem = scene->m_operatorDropItem;
+    if (!operatorRule->m_isStart) {
+        return false;
+    }
+    if (operatorDropItem != NULL && operatorDropItem->m_isSuddenDeath) {
+        return true;
+    }
+    if (operatorRule->m_isGameSet) {
+        return false;
+    }
     if (operatorRule->m_remainingFrameTime < 3600) {
         return true;
     }
 
+    int activeFighterCount = 0;
+    bool pinchFighterFound = false;
     for (int i = 0; i < g_ftManager->getEntryCount(); i++) {
         int entryId = g_ftManager->getEntryIdFromIndex(i);
         if (g_ftManager->isFighterActivate(entryId, -1)) {
-            if (g_ftManager->getOwner(entryId)->getStockCount() == 1) {
-                return true;
+            if (g_ftManager->getOwner(entryId)->getStockCount() > 0) {
+                activeFighterCount++;
+                if (activeFighterCount > 2) {
+                    return false;
+                }
+            }
+            if (!pinchFighterFound && g_ftManager->getOwner(entryId)->getStockCount() == 1 && (!g_ftManager->m_isStamina || g_ftManager->getOwner(entryId)->getHitPoint() < 100)) {
+                pinchFighterFound = true;
             }
         }
     }
 
-    return false;
+    return pinchFighterFound;
 }
