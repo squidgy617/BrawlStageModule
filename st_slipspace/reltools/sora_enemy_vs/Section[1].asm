8c66d2a4fa4e315410429e220a998ce4abfbe45e
squidgy617 <ryantheroux@yahoo.com>
2025-08-17 20:29:58 -0500

    Added Cymul

--- st_slipspace/reltools/sora_enemy_vs/Section[1].asm
+++ st_slipspace/reltools/sora_enemy_vs/Section[1].asm
@@ -47104,7 +47104,7 @@ wnemSimple_Data____ct1:
 #include "./Enemies/asm/emPacci.asm"
 #include "./Enemies/asm/emJyakeel.asm"
 #include "./Enemies/asm/emAroaros.asm"
-#include "./Enemies/asm/emCymal.asm"
+.include "./Enemies/asm/emCymal.asm"
 #include "./Enemies/asm/emDeathpod.asm"
 #include "./Enemies/asm/emDekakuribo.asm"
 #include "./Enemies/asm/emBucyulus.asm"
