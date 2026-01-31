6404aa3d2200f51437168a5a7a78b4ce27d91ac1
squidgy617 <ryantheroux@yahoo.com>
2026-01-31 17:50:33 -0600

    Update call to createObjPokeTrainer

--- st_slipspace/source/st_slipspace.cpp
+++ st_slipspace/source/st_slipspace.cpp
@@ -776,7 +776,8 @@ void stSlipspace::createObj()
     nw4r::g3d::ResFileData* scnData = static_cast<nw4r::g3d::ResFileData*>(m_fileData->getData(Data_Type_Scene, 0, 0xfffe));
     registScnAnim(scnData, 0);
     initPosPokeTrainer(1, 0);
-    createObjPokeTrainer(m_fileData, 0x65, "PokeTrainer00", this->m_pokeTrainerPos, 0x0);
+    Vec3f vector = Vec3f(0,0,0);
+    createObjPokeTrainer(m_fileData, 0x65, "PokeTrainer00", this->m_pokeTrainerPos, &vector);
     this->pokeTrainerGround = static_cast<grPokeTrainer*>(this->getGround(this->getGroundNum() - 1));
 
     stSlipspaceData* stageData = static_cast<stSlipspaceData*>(this->m_stageData);
